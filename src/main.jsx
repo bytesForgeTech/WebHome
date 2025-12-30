ec0cde1c0bbf57b72ca0ecaf31f9861d9e6a638e
Anurag Biswas <anuragbiswas512@gmail.com>
2025-12-30 18:21:10 +0530

    added darkmode

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,17 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { RouterProvider, createRouter } from '@tanstack/react-router';
+import { routeTree } from './routeTree.gen';
+import './index.css';
+
+// Create a new router instance
+const router = createRouter({
+    routeTree,
+    basepath: '/WebHome',
+});
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <StrictMode>
+        <RouterProvider router={router} />
+    </StrictMode>
+);
